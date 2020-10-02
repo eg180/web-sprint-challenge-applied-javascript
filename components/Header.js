9a02016693e9118fca9046bb59a72d5720fc6cc4
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-02 11:42:26 -0400

    Completed header.js tasks

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // guts parent 
+    const headerr = document.createElement('header');
+    // end of guts parent
+
+    const spanDate = document.createElement('span');
+    spanDate.classList.add('date');
+    spanDate.textContent = 'March 28, 2020';
+    headerr.appendChild(spanDate);
+    
+    const h1Title = document.createElement('h1');
+    h1Title.textContent = 'Lambda Times';
+    headerr.appendChild(h1Title);
+
+    spanTemp = document.createElement('span');
+    spanTemp.classList.add('temp');
+    spanTemp.textContent = '98°';
+    headerr.appendChild(spanTemp);
+
+    // entry point 
+    headerContainer = document.querySelector('.header-container');
+
+    // append our div to the entry point
+    headerContainer.appendChild(headerr);
+
+
+};
+
+Header();
