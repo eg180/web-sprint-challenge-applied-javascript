b9b4ef855bfa2dbcc1c190e342a255f9a969b799
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-02 13:53:57 -0400

    Semi-final product

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,81 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+arcticlePromise = axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(res => {
+    // call the function created below HERE
+    // const bootstrapArray = res.data.articles.bootstrap;
+    // console.log(bootstrapArray[0].headline);
+
+    const bootstrapArray = res.data.articles.bootstrap;
+    // const jsHeadlinesArray = res.data.articles.javascript;
+    // const techHeadlinesArray = res.data.articles.technology;
+    // const jqueryArray = res.data.articles.jquery;
+    // const nodejsArray = res.data.articles.node;
+
+    // resArray = [bootstrapArray, jsHeadlinesArray, techHeadlinesArray, jqueryArray, nodejsArray];
+
+    // resArray.forEach(obj => {
+    //     let singleCardDiv = cardCreatorFromRes(obj);
+    //     cardsContainerParent.appendChild(singleCardDiv);
+    let singleCardDiv = cardCreatorFromRes(bootstrapArray);
+    cardsContainerParent.appendChild(singleCardDiv);
+    })
+
+    // const authorCard = cardCreatorFromRes(res);
+    // cardsContainerParent.append(authorCard);
+// })
+.catch(err => {
+    // handle error
+});
+
+
+// WHERE YOU WILL APPEND EVERYTHING ONCE ALL THE CODE RUNS
+const cardsContainerParent = document.querySelector('cards-container');
+// DO NOT TOUCH
+
+
+
+function cardCreatorFromRes(resObject) {
+
+
+    
+
+    resObject.forEach((element, indx) => {
+        let singleCardDiv = document.createElement('div');
+        singleCardDiv.classList.add('card');
+
+        let singleCardHeadline = document.createElement('div');
+        singleCardHeadline.textContent = resObject[indx].headline;
+        singleCardHeadline.classList.add('headline');
+
+        singleCardDiv.appendChild(singleCardHeadline);
+
+
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+
+        singleCardDiv.appendChild(authorDiv);
+
+
+
+        let imgContainterDiv = document.createElement('div');
+        imgContainterDiv.classList.add('img-container');
+        authorDiv.appendChild(imgContainterDiv);
+
+    
+        const imgOfAuthor = document.createElement('img');
+        imgOfAuthor.src = resObject[indx].authorPhoto;
+        imgContainterDiv.appendChild(imgOfAuthor);
+
+
+        const authorNameSpan = document.createElement('span');
+        authorNameSpan.textContent = resObject[indx].authorName;
+        authorDiv.appendChild(authorNameSpan);
+
+        // append it all to parent div
+
+        
+    }); return singleCardDiv;
+};
